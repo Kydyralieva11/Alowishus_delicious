f7a0b9d34d6cbe1f3f5365bb6d88f85a171e316d
Kydyralieva11 <117450496+Kydyralieva11@users.noreply.github.com>
2023-03-17 18:15:31 +0600

    fixing the swiper problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    mousewheel: true,
-    keyboard: true,
+// var swiper = new Swiper(".mySwiper", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// });
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 30,
+  breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      720: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
